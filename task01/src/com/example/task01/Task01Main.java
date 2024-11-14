c35fb08da525300e46fc144a0ef61df07b0ea131
Deeplerg <deeplerg@gmail.com>
2024-11-14 23:58:39 +0500

    Complete tasks

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,42 @@
 package com.example.task01;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
-        System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current absolute path is: " + s);
+        System.out.println(extractSoundName(new File("task01\\src\\main\\resources\\3727.mp3")));
+
     }
 
-    public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+    public static String extractSoundName(File file) throws IOException {
+        String[] command = buildCommand(file);
+
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.command(command);
+        builder.redirectOutput(ProcessBuilder.Redirect.PIPE);
+        Process process = builder.start();
+
+        InputStream processOutput = process.getInputStream();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(processOutput)))
+        {
+            return reader.readLine();
+        }
+    }
+
+    private static String[] buildCommand(File file) {
+        String commandFormat = "ffprobe -v error -of default=nw=1:nk=1 -show_entries format_tags=title %s";
+
+        String filename = file.getAbsolutePath();
+        String formattedCommand = String.format(commandFormat, filename);
+
+        return formattedCommand.split("\\s+");
     }
 }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,52 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
-        System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
+        List<Path> paths = listFiles(Paths.get("task02/src/main/resources/"));
+        System.out.println(paths);
 
     }
 
-    public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+    public static List<Path> listFiles(Path rootDir) throws IOException {
+        try (Stream<Path> pathStream = Files.walk(rootDir)) {
+            return pathStream
+                    .filter(Files::isRegularFile)
+                    .collect(Collectors.toList());
+        }
     }
+
+
+    // simple
+//    public static List<Path> listFiles(Path rootDir) {
+//        ArrayList<Path> paths = new ArrayList<>();
+//
+//        try (Stream<Path> pathStream = Files.list(rootDir)) {
+//            pathStream.forEach(path -> {
+//                if(Files.isDirectory(path)) {
+//                    try {
+//                        paths.addAll(listFiles(path));
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                }
+//                if(Files.isRegularFile(path))
+//                    paths.add(path);
+//            });
+//        }
+//
+//        return paths;
+//    }
 }
